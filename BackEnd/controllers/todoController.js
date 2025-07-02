d0546fcdd3fb5306ee49d3e49f4aa58a3932d6ba
ayirezang <patienceayirezang33@gmail.com>
2025-07-02 20:38:38 +0000

    updateController

--- BackEnd/controllers/todoController.js
+++ BackEnd/controllers/todoController.js
@@ -1,5 +1,5 @@
 const todoModel = require("../models/todoModel");
-
+// create
 const createTodo = async (req, res) => {
   const { task, description } = req.body;
 
@@ -22,7 +22,51 @@ const retrieveTodo = async (req, res) => {
     res.status(500).json({ message: "failed to fetch" });
   }
 };
+//update
+const updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("updating with id", id);
+    //data from req body
+    const updateTodo = req.body;
+    const updatedTodo = await todoModel.findByIdAndUpdate(id, updateTodo, {
+      new: true,
+    });
+    if (!updatedTodo) {
+      return res.status(404).json({ message: "not found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "updated successfully", data: updatedTodo });
+  } catch (error) {
+    console.error("error:", error);
+    return res
+      .status(400)
+      .json({ message: "no id found", error: error.message });
+  }
+};
+//delete
 
+const deleteTodo = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log("deleting with id", id);
+    const deletedTodo = await todoModel.findByIdAndDelete(id);
+    if (!deletedTodo) {
+      console.log("todo not found with id", id);
+      return res
+        .status(400)
+        .json({ message: "todo not found", data: deletedTodo });
+    }
+    console.log("successfully deleted", deleteTodo);
+    return res.json({ message: "deleted successfully", data: deletedTodo });
+  } catch (error) {
+    console.error("error:", error);
+    return res.status(500).json({ message: "error", error: error.message });
+  }
+};
+//update
 
-// 
-module.exports = { createTodo, retrieveTodo };
+//
+
+module.exports = { createTodo, retrieveTodo, deleteTodo, updateTodo };
--- BackEnd/routes/todoRoutes.js
+++ BackEnd/routes/todoRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const todoModel = require("../models/todoModel");
-const { createTodo, retrieveTodo } = require("../controllers/todoController");
+const {
+  createTodo,
+  retrieveTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todoController");
 
 router.post("/todos", createTodo);
 router.get("/todos", retrieveTodo);
+router.put("/todos/:id", updateTodo);
+router.delete("/todos/:id", deleteTodo);
 
 module.exports = router;
