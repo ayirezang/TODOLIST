90af81e0c617e8fdd89c126daeac7898238cae14
ayirezang <patienceayirezang33@gmail.com>
2025-07-12 22:35:48 +0000

    vercel.json

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        
         <Route
           path="/todopage"
           element={
