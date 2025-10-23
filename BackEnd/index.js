963eb418b817306927514ff623664dc0e233e2d5
ayirezang <patienceayirezang33@gmail.com>
2025-10-23 15:00:06 +0000

    backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,22 +8,21 @@ const userRoutes = require("./routes/userRoutes");
 const todoRoutes = require("./routes/todoRoutes");
 
 const server = express();
-server.use(cors(
-  
-));
+server.use(
+  cors({
+    origin: ["http://localhost:5173", "https://todolist-fgyu.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.use("/api", todoRoutes);
 
 server.use("/api", userRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-//   server.use(express.static(path.join(__dirname, "/frontend/dist")));
-//   server.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
 
 mongoose
   .connect(process.env.MONGO_DB)
